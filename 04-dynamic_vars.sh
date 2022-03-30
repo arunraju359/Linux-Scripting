05df4151f459c4545b127353169b6c7e4e6b8e21
manu <manu@local.com>
2022-03-30 08:56:36 +0530

    test

--- 04-dynamic_vars.sh
+++ 04-dynamic_vars.sh
@@ -4,3 +4,6 @@ DATECMD="$(date +%F)"
 # Very bad way . Always dynamic values should not be hardcoded. They should be able to fetch dynamically.
 
 echo "Today's date is : $DATECMD"
+
+echo "Number of usres connected to the system : $(who | wc -l) "
+
