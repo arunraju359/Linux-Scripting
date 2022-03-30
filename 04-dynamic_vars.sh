d9bf5a2f222cb7bcc659fe8991ad27d3ad871bd6
manu <manu@local.com>
2022-03-30 08:52:30 +0530

    test

--- 04-dynamic_vars.sh
+++ 04-dynamic_vars.sh
@@ -1,4 +1,6 @@
 #!/bin/bash 
 
-DATE="30-Mar-2022"
-echo $DATE 
+DATECMD="$(date +%F)"
+# Very bad way . Always dynamic values should not be hardcoded. They should be able to fetch dynamically.
+
+echo "Today's daye is : $DATECMD"
