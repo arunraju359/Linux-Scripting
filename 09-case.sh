7d49bc6cad5a4faa03c9dcc4868467a8841c0c2c
manu <manu@local.com>
2022-03-31 08:34:19 +0530

    --- Auto-Git Commit ---

--- 09-case.sh
+++ 09-case.sh
@@ -21,6 +21,6 @@ case $ACTION in
         echo "Stopping ABC Service"
         ;; 
     *)
-        echo -e "\eProvided input is invalid : Enter either start OR stop options only"
+        echo -e "\e[31m Provided input is invalid : Enter either start OR stop options only"
 esac 
 
