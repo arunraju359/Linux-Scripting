bb2028a25856e60e97607a1e30f75cc90b5a19e7
manu <manu@local.com>
2022-04-01 08:00:52 +0530

    --- Auto-Git Commit ---

--- stack.sh
+++ stack.sh
@@ -22,12 +22,6 @@ yum install httpd -y &> /tmp/stack.log
 echo -n "Create Proxy Config"
 echo 'ProxyPass "/student" "http://localhost:8080/student" 
 ProxyPassReverse "/student"  "http://localhost:8080/student"' > /etc/httpd/conf.d/proxy.conf 
-if [ $? -eq 0 ]; then
-    echo -e "\e[32m Success \e[0m "
-else 
-    echo -e "\e[31m Failure \e[0m "
-fi 
-
 
 echo -n "Setup student index file"
 curl -s  https://devops-cloudcareers.s3.ap-south-1.amazonaws.com/index.html -o /var/www/html/index.html
