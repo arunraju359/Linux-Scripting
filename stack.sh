b3628c58c522f05f61c12a5766efbeef79120a91
manu <manu@local.com>
2022-04-01 08:17:52 +0530

    --- Auto-Git Commit ---

--- stack.sh
+++ stack.sh
@@ -45,7 +45,7 @@ stat $?
 
 echo -n "Creating $FUSER Functional User : "
 id $FUSER  &>> $LOG 
-if [ $1 -eq 0 ]; then
+if [ $? -eq 0 ]; then
     echo -e "\e[32m Success \e[0m "
 else 
     echo -e "\e[31m Failure \e[0m "
