42888001ca2b3acb01d3c6d77bf409de7ea97217
manu <manu@local.com>
2022-04-01 07:45:44 +0530

    --- Auto-Git Commit ---

--- stack.sh
+++ stack.sh
@@ -3,7 +3,7 @@
 ID=$(id -u)
 
 if [ $ID -ne 0 ] ; then
-    echo -e "You need to execure this script as a root user or with a sudo command"
+    echo -e "\e[31m You need to execure this script as a root user or with a sudo command"
 fi 
 
 yum install httpd -y 
