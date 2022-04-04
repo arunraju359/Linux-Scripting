3ca1c6d951cdbf8adc9f6d9b1cfa2b78cabd08d0
manu <manu@local.com>
2022-04-04 08:30:49 +0530

    --- Auto-Git Commit ---

--- stack.sh
+++ stack.sh
@@ -60,7 +60,7 @@ fi
 echo -n "Downloading the tomcat :"
 cd /home/$FUSER
 wget $TOMCAT_URL &>> $LOG && tar -xf apache-tomcat-${TOMCAT_VERSION}.tar.gz  &>> $LOG
-chown -R $FUSER:$FUSER apache-tomcat-$APACHE_VERSION &>> $LOG
+chown -R $FUSER:$FUSER apache-tomcat-$TOMCAT_VERSION &>> $LOG
 stat $? 
 
 echo -n "Downloading the WAR File : "
