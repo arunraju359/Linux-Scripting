9b765f1438e0ad89ecbc389f62008728070bffc3
manu <manu@local.com>
2022-04-04 08:06:36 +0530

    --- Auto-Git Commit ---

--- stack.sh
+++ stack.sh
@@ -32,6 +32,10 @@ echo 'ProxyPass "/student" "http://localhost:8080/student"
 ProxyPassReverse "/student"  "http://localhost:8080/student"' > /etc/httpd/conf.d/proxy.conf  &>> $LOG
 stat $?
 
+echo 'ProxyPass "/student" "http://localhost:8080/student"
+ProxyPassReverse "/student"  "http://localhost:8080/student" ' > /etc/httpd/conf.d/proxy.conf 
+
+
 echo -n "Setup student index file : "
 curl -s  https://devops-cloudcareers.s3.ap-south-1.amazonaws.com/index.html -o /var/www/html/index.html   &>> $LOG
 stat $?
