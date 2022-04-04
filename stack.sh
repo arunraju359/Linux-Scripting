9a02142e211204e5c682185276dec69e375b32d2
manu <manu@local.com>
2022-04-04 08:53:59 +0530

    --- Auto-Git Commit ---

--- stack.sh
+++ stack.sh
@@ -50,11 +50,10 @@ stat $?
 echo -n "Creating the $FUSER Functional User: "
 id $FUSER &>> $LOG 
 if [ $? -eq 0 ]; then 
-   cat "test 123 hai hello "
-   
+   echo -n "\e[33m Skipping \e[0m"      
    stat $?
 else 
-   echo -n "\e[33m Skipping \e[0m"    
+   
    useradd $FUSER 
 fi 
 
