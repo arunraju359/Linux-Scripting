ecaaeb873ca151e2c0dd001c65de464ce197527f
manu <manu@local.com>
2022-04-05 07:56:18 +0530

    --- Auto-Git Commit ---

--- stack.sh
+++ stack.sh
@@ -89,7 +89,7 @@ systemctl start  mariadb  &>> $LOG
 stat $?
 
 echo -n "Injecting he schema : "
-mysql <  /tmp/studentapp.sql 
+mysql <  /tmp/studentapp.sql
 
 echo -n "Starting Tomcat: "
 sh bin/startup.sh  &>> $LOG
