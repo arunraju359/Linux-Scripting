6f88bdef0c5205c1a185b7faec737ceca89fbcce
manu <manu@local.com>
2022-04-05 08:11:58 +0530

    --- Auto-Git Commit ---

--- stack.sh
+++ stack.sh
@@ -80,7 +80,7 @@ stat $?
 
 echo -n "Downloading DB Schema : "
 curl -s $SCHEMA_URL > /tmp/studentapp.sql &>> $LOG
-stat $
+stat $?
 
 # # Setting up MariaDB 
 echo -n "Installing & Starting Mariadb: "
